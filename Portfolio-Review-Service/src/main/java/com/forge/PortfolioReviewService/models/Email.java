4962f86d2979b61437a013f99ac73000707c3ef4
Faberim <jfaberim@gmail.com>
2020-10-24 12:44:37 -0500

    Completed functionality for sending email to mailtrap

---
+++ Portfolio-Review-Service/src/main/java/com/forge/PortfolioReviewService/models/Email.java
@@ -0,0 +1,28 @@
+package com.forge.PortfolioReviewService.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Email {
+	
+	private String userFirstName;
+	
+	private String userEmail;
+	
+	private String subject;
+	
+	private String portfolioStatus;
+	
+	private String feedBack;
+	
+	private int portfolioId;
+	
+	private String adminUserName;
+	
+	
+	
+}
