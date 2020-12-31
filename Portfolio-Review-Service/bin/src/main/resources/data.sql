bbafe583ad7fc7029ee1017419576c74bcb3e7f3
Isa <isakaybeckman@gmail.com>
2020-12-31 10:47:09 -0600

    added bin to gitignore part2

--- Portfolio-Review-Service/bin/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into user_table values (1,'admin@revature.net','adminFirst',true,'adminLast','admin');
-insert into user_table values (2,'user@revature.net','FirstName',false,'LastName','password');
